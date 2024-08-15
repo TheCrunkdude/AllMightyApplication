f2a19b409d39389bc4e0e5e2a412ea77a860e84f
TheCrunkdude <chris.contreras.qro@hotmail.com>
2024-08-14 20:24:34 -0600

    Feature/Add buttons
    Ligue las rutas a App.routes
    En component.html cree los botones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 import { ButtonModule } from '@syncfusion/ej2-angular-buttons'
 import { TestComponentComponent } from "./Testing-Suite/test-component/test-component.component";
@@ -7,7 +7,7 @@ import { TestComponentComponent } from "./Testing-Suite/test-component/test-comp
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule, TestComponentComponent], 
+  imports: [RouterOutlet, ButtonModule,RouterLink, TestComponentComponent], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes,  } from '@angular/router';
+import { TestComponentComponent } from './Testing-Suite/test-component/test-component.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: 'Test', component: TestComponentComponent },
+];
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutes { }
